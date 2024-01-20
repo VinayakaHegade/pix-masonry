9a122c13461ac1d9b662cbd26bf9881f5cb340e0
Vinayak <myselfvinayakhegde@gmail.com>
2024-01-21 01:18:30 +0530

    style: refactor header styles

--- app/ui/header/header.js
+++ app/ui/header/header.js
@@ -3,24 +3,22 @@ import styles from "./header.module.css";
 
 const Header = () => {
   return (
-    <div className={styles.wrapper}>
-      <header className={styles.header}>
-        <Link href="/" className="medium-text">
-          Homepage
+    <header className={styles.header}>
+      <Link href="/" className="medium-text">
+        Homepage
+      </Link>
+      <div className={styles.authLinks}>
+        <Link href="/login" className="medium-text">
+          Login
         </Link>
-        <div className={styles.authLinks}>
-          <Link href="/login" className="medium-text">
-            Login
-          </Link>
-          <Link
-            href="/register"
-            className={`${styles.createAccount} medium-text`}
-          >
-            Create Account
-          </Link>
-        </div>
-      </header>
-    </div>
+        <Link
+          href="/register"
+          className={`${styles.createAccount} medium-text`}
+        >
+          Create Account
+        </Link>
+      </div>
+    </header>
   );
 };
 
